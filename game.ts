e5663d4825ad62c955cfce24e3555b2f05e820d2
Moosanoor24 <159759573+Moosanoor24@users.noreply.github.com>
2024-03-29 02:37:13 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomnumber = Math.floor(Math.random() * 7 + 1);
+const answer = await inquirer.prompt([
+    {
+    name: "userguessnumber",
+    type: "number",
+    message: "please guess a number between 1-7",
+    }
+]);
+if(answer.userguessnumber===randomnumber) {
+    console.log(chalk.green.bold("Congratulation you guess right number,"));
+    
+}else{
+    console.log(chalk.red.bold("Oh! badluck please try again"));
+};
